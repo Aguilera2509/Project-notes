da52386100ade811697b8f21c6a7641c3c2758e8
José Aguilera <ja846699@gmail.com>
2021-04-19 14:28:46 -0400

    Falta almacenar la info e enviarla

---
+++ botones.js
@@ -0,0 +1,14 @@
+//Funcioón de Saludo
+let saludo = function()
+{
+    nombre = prompt("Introduce tu nombre");
+
+    alert("Hola, bienvenido, me da mucho gusto tenerte aquí conmigo, " 
+    + nombre + " . Espero disfrutes dejando una nota, si quedan vacias claro esta, no sobreescribas la de otros. :D");
+}
+
+//Función de Despedida
+let despedida = function()
+{
+    alert("Fue un gusto que visitarás mi primera página, espero que tengas un buen día. :D");
+}
